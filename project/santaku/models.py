3dc1dc9d513dd33bc8372e6fa9eb6e530c3b1cfb
MinagawaTetsunori <soru1115@gmail.com>
2025-02-22 15:06:00 +0000

    unpdate models class

--- project/santaku/models.py
+++ project/santaku/models.py
@@ -1,19 +1,7 @@
 from django.db import models
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-
-
-
-
+CHAR_DEFAULT_MAX_LENGTH = 20
 
 class CustomerBot(models.Model):
     '''
@@ -22,47 +10,57 @@ class CustomerBot(models.Model):
     name = models.CharField(max_length=20)
 
 
-class MerchantBot(models.Model):
-    '''
-    商人役のボットデータ最上部
-    '''
-    name = models.CharField(max_length=20)
-    clear_conciliation_point = \
-        conciliation_add_point = models.IntegerField(default=0)
-
-
 class CustomerBotFlow(models.Model):
     '''
     客人役の対話フロー
     '''
     customer_bot_id = models.ForeignKey(CustomerBot, on_delete=models.CASCADE)
-    talk_text = models.CharField(max_length=20)
+    talk_text = models.CharField(max_length=CHAR_DEFAULT_MAX_LENGTH)
+
+    # sはsantakuの頭文字
+    s1_name = models.CharField(
+        max_length=CHAR_DEFAULT_MAX_LENGTH, default='選択肢名1')
+    s1_conciliation_add_point = models.IntegerField(default=0)  # 懐柔(ゲームでは怪柔)
+    s1_res = models.CharField(
+        max_length=CHAR_DEFAULT_MAX_LENGTH, default='客役の応答1')
+    s1_branch = models.IntegerField(default=0)
+    s2_name = models.CharField(
+        max_length=CHAR_DEFAULT_MAX_LENGTH, default='選択肢名2')
+    s2_conciliation_add_point = models.IntegerField(default=0)
+    s2_res = models.CharField(
+        max_length=CHAR_DEFAULT_MAX_LENGTH, default='客役の応答2')
+    s2_branch = models.IntegerField(default=0)
+    s3_name = models.CharField(
+        max_length=CHAR_DEFAULT_MAX_LENGTH, default='選択肢名3')
+    s3_conciliation_add_point = models.IntegerField(default=0)
+    s3_res = models.CharField(
+        max_length=CHAR_DEFAULT_MAX_LENGTH, default='客役の応答3')
+    s3_branch = models.IntegerField(default=0)
 
 
-class CustomerBotFlowSantaku(models.Model):
+class MerchantBot(models.Model):
     '''
-    客人役に対する商人の三択
+    商人役のボットデータ最上部
     '''
-    customer_bot_flow_id = \
-        models.ForeignKey(CustomerBotFlow, on_delete=models.CASCADE)
-    conciliation_add_point = models.IntegerField()  # 懐柔(ゲームでは怪柔)
+    name = models.CharField(max_length=20)
+    clear_conciliation_point = models.IntegerField(default=0)
 
 
 class MerchantBotBrain(models.Model):
     '''
     商人役の行動選択ルール
     '''
-    merchant_bot_id = models.ForeignKey(CustomerBot, on_delete=models.CASCADE)
-    main_cmd = models.CharField(max_length=20)
-    priority = models.IntegerField()
+    merchant_bot_id = models.ForeignKey(MerchantBot, on_delete=models.CASCADE)
+    main_cmd = models.CharField(max_length=CHAR_DEFAULT_MAX_LENGTH)
+    priority = models.IntegerField(default=0)
 
 
 class Conduct(models.Model):
     '''
     行動選択肢データの最上部
     '''
-    display_name = models.CharField(max_length=20, '表示用の名前')
-    meta_name = models.CharField(max_length=20, 'function name only')
+    display_name = models.CharField(max_length=20, default='表示用の名前')
+    meta_name = models.CharField(max_length=20, default='function name only')
     rate = models.IntegerField(default=1)
     note = models.TextField(default='ここに説明を追加')
 
