11b46ad30614a48eebe947d9b9b7c0887a3db01a
MinagawaTetsunori <soru1115@gmail.com>
2025-02-25 16:44:54 +0000

    creat sample1 hint bases

--- project/santaku/original/sample1.py
+++ project/santaku/original/sample1.py
@@ -1,10 +1,32 @@
 # 1体分
 
+HINT_KAIJU_POINTS = {
+    '目的': {
+        '修復': {'nega': 0, 'neut': 0, 'posi': 0},
+        '成長': {'nega': 0, 'neut': 1, 'posi': 0},
+        '復讐': {'nega': 1, 'neut': 1, 'posi': 0}
+    },
+    '対象者': {
+        '自分': {'nega': 0, 'neut': 0, 'posi': 0},
+        '親しい人': {'nega': 1, 'neut': 0, 'posi': 1},
+        'その他': {'nega': 1, 'neut': 0, 'posi': 0}
+    },
+    '要求対応': {
+        '共感': {'nega': 0, 'neut': 0, 'posi': 0},
+        'アドバイス': {'nega': 0, 'neut': 1, 'posi': 1},
+        '後押し': {'nega': 0, 'neut': 0, 'posi': 1}
+    }
+}
+
+
+
+
+
 def game():
     '''
     ゲームのメイン部分
     '''
-    flow = [
+    FLOW = [
         {'talk': 'aaaa', 'santaku': ['a', 'b', 'c']}
     ]
 
